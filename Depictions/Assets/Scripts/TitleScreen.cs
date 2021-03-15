a91572fdcc43c1d99dca232445219c880fbcd18b
ZacharyShirikjian <zshirikj@student.fitchburgstate.edu>
2021-03-15 11:54:11 -0400

    Began working on Title Screen
    Title Screen works :D

---
+++ Depictions/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+
+    //Load the Tutorial Level (with the build index of 1) 
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    //If the player taps on the Options button,
+    //Load up the options (Add in Panel later) 
+    public void OpenOptions()
+    {
+        //Add in code to pop up a panel for adding in the options 
+    }
+}

6c9a042b3ce36cf638bebbfb2cc1993a242b4708
TJ_Yads <tyadisernia@icloud.com>
2021-03-25 14:07:35 -0400

    modified rotate and mirror, better walls

--- Depictions/Assets/Scripts/Rotatable.cs
+++ Depictions/Assets/Scripts/Rotatable.cs
@@ -7,10 +7,13 @@ public class Rotatable : MonoBehaviour
 {
     //tj
     //edit made so that the rotation will affect the parent game object--allows the new mirrors to work
+    //modified to allow turning of new mirrors and set default turning points as 180 degree
     public Canvas myCanvas;
 
-    public float rotateAngle = 90f;
+    public float rotateAngle = 180f;
+    public float rotateAngle2 = 270f;
     public Quaternion target;
+    private bool Return;
     bool moving;
 
     void Start()
@@ -33,6 +36,18 @@ public class Rotatable : MonoBehaviour
         target = transform.rotation;
         target.y = transform.rotation.y + rotateAngle;
         transform.parent.Rotate(0f, rotateAngle, 0f);
+        if(Return)
+        {
+            rotateAngle2 = rotateAngle2 * -1;
+            transform.Rotate(0f, rotateAngle2, 0f);
+            Return = false;
+        }
+        else
+        {
+            rotateAngle2 = rotateAngle2 * -1;
+            transform.Rotate(0f, rotateAngle2, 0f);
+            Return = false;
+        }
     }
 
     public void RotateLeft()
@@ -40,5 +55,17 @@ public class Rotatable : MonoBehaviour
         target = transform.rotation;
         target.y = transform.rotation.y - rotateAngle;
         transform.parent.Rotate(0f,-rotateAngle,0f);
+        if (Return)
+        {
+            rotateAngle2 = rotateAngle2 * -1;
+            transform.Rotate(0f, rotateAngle2, 0f);
+            Return = false;
+        }
+        else
+        {
+            rotateAngle2 = rotateAngle2 * -1;
+            transform.Rotate(0f, rotateAngle2, 0f);
+            Return = false;
+        }
     }
 }

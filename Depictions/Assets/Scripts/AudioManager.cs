bbd50d759cfc3eaed0c23612910f56d052514c9a
ZacharyShirikjian <zshirikj@student.fitchburgstate.edu>
2021-03-29 11:36:21 -0400

    Added StonePushSFX to Bust

--- Depictions/Assets/Scripts/AudioManager.cs
+++ Depictions/Assets/Scripts/AudioManager.cs
@@ -10,8 +10,13 @@ public class AudioManager : MonoBehaviour
     public AudioClip playerFootstep;
     public AudioClip playerLose;
     public AudioClip rotateObjectSFX;
-    public AudioClip stonePushSFX; 
+    public AudioClip stonePushSFX;
 
+    //Reference to this AudioSource 
+    public AudioSource source;
+
+    //An instance of this AudioManager script, which is used for the DontDestroyOnLoad method.
+    private static AudioManager instance; 
     // Start is called before the first frame update
     void Start()
     {

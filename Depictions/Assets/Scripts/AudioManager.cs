b1ccbe387326df326baf24001eb29d5b8dff2918
TJ_Yads <tyadisernia@icloud.com>
2021-03-30 19:49:20 -0400

    Revert "Merge branch 'master' into WallFix"
    This reverts commit e96faa7f7bf5214aa7005dd74f4e2e96b57e939d.

---
+++ Depictions/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioClip cratePushSFX;
+    public AudioClip doorOpenSFX;
+    public AudioClip playerFootstep;
+    public AudioClip playerLose;
+    public AudioClip rotateObjectSFX;
+    public AudioClip stonePushSFX;
+
+    //Reference to this AudioSource 
+    public AudioSource source;
+
+    //An instance of this AudioManager script, which is used for the DontDestroyOnLoad method.
+    private static AudioManager instance; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

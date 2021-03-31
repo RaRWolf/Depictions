ff8482df7435a682bdef57710771fcdc1c29afe8
ZacharyShirikjian <zshirikj@student.fitchburgstate.edu>
2021-03-31 10:03:48 -0400

    MUSIC MANAGER WORKS AGAIN

--- Depictions/Assets/Scripts/MusicManager.cs
+++ Depictions/Assets/Scripts/MusicManager.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    //Reference to this Audio Source
+    public AudioSource source;
+
+    //Reference to music source 
+    //public AudioSource musicSource;
+
+    //An instance of this AudioManager script, which is used for the DontDestroyOnLoad method. 
+    private static MusicManager instance;
+
+
+    private void Awake()
     {
-        
+        //If there is already an instance of this script in the current scene, then delete this duplicated instance 
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        //Otherwise, if there is no other instances of this script in the current scene, then make sure that it doesn't get deleted with DontDestroyOnLoad()
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(transform.gameObject); //DontDestroyOnLoad is applied to the current gameObject with the AudioManager script on it 
+        }
     }
 }
